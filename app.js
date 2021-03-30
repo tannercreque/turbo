5a4cd0dd58a8609d57faa94d094d278830e1a9b6
Jblack017 <blackhamjoshua@comcast.net>
2021-03-30 15:26:48 -0600

    style updates

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hey")
+console.log("hey");
+
+const buttonTest = document.querySelector("button");
+console.log(buttonTest);
+buttonTest.addEventListener("click", redirect());
+
+function redirect() {
+  console.log("hey");
+}
